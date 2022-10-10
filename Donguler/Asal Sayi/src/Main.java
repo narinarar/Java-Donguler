05b93371d0d39ec55108879c49caa2410cd2e412
Narin Arar <narinarar1@gmail.com>
2022-10-10 22:11:56 +0300

    Dongu

---
+++ Donguler/Asal Sayi/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+
+        for (i = 1; i < 100; i++) {
+            if (i % i == 0) {
+
+                System.out.print("Asal Sayılar: ");
+                System.out.println(i);
+            }
+        }
+    }
+}
